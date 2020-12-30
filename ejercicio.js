945e4914bb35007f24811e376706f923a5fb607a
m4niga <agustinguerr4@gmail.com>
2020-12-29 22:44:27 -0300

    Improves

--- ejercicio.js
+++ ejercicio.js
@@ -1,110 +1,111 @@
+class Vehiculo {
+  constructor(marca, modelo, precio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.precio = precio;
+  }
 
-      class Vehiculo {
-        constructor(marca, modelo, precio) {
-          this.marca = marca;
-          this.modelo = modelo;
-          this.precio = precio;
-        }
-      }
-      class Motocicleta extends Vehiculo {
-        constructor(marca, modelo, precio, cilindrada) {
-          super(marca, modelo, precio);
-          this.cilindrada = cilindrada;
-        }
-      }
-      class Automovil extends Vehiculo {
-        constructor(marca, modelo, precio, puertas) {
-          super(marca, modelo, precio);
-          this.puertas = puertas;
-        }
-      }
-      function loadData() {
-        return (data = [
-            new Automovil("Peugeot", "206", 200000.0, 4),
-            new Motocicleta("Honda", "Titan", 60000.0, "125cc"),
-            new Automovil("Peugeot", "208", 250000.0, 5),
-            new Motocicleta("Yamaha", "YBR", 80500.5, "160cc")
-          ])
-      }
+  contieneY() {
+    if (this.modelo.includes("Y")) {
+      return console.log(`${this.marca} ${this.modelo} ${this.precio}`);
+    }
+  }
 
-      const data = loadData();
+  printData() {
+    this.cilindrada
+      ? console.log(`Marca: ${this.marca} // Modelo: ${this.modelo} // Precio: ${formatearPrecio(this.precio)} // Cilindrada: ${this.cilindrada}`)
+      : console.log(`Marca: ${this.marca} // Modelo: ${this.modelo} // Precio: ${formatearPrecio(this.precio)} // Puertas: ${this.puertas}`);
+  }
+}
+class Motocicleta extends Vehiculo {
+  constructor(marca, modelo, precio, cilindrada) {
+    super(marca, modelo, precio);
+    this.cilindrada = cilindrada;
+  }
+}
+class Automovil extends Vehiculo {
+  constructor(marca, modelo, precio, puertas) {
+    super(marca, modelo, precio);
+    this.puertas = puertas;
+  }
+}
 
-      function printAll(data) {
-        data.map((item, index) => {
-          var element = "";
-          for (const property in item) {
-            element += `${property}: ${item[property]} // `;
-          }
-          console.log(element);
-        });
-      }
+function formatearPrecio(precio) {
+  const precioFormateado = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  }).format(precio);
+  return precioFormateado;
+}
 
-      function masCaro(data) {
-        var caro = 0;
-        data.map((item, index) => {
-          if (item.precio > caro) {
-            caro = item.precio;
-            masCaro = item.marca + " " + item.modelo;
-          }
-        });
-        console.log("Vehículo mas caro:", masCaro);
-      }
+function loadData() {
+  const data = [
+    new Automovil("Peugeot", "206", 200000.0, 4),
+    new Motocicleta("Honda", "Titan", 60000.0, "125cc"),
+    new Automovil("Peugeot", "208", 250000.0, 5),
+    new Motocicleta("Yamaha", "YBR", 80500.5, "160cc")
+  ];
+  return data;
+}
 
-      function masBarato(data) {
-        var barato = 0;
-        data.map((item, index) => {
-          if (barato == 0 || item.precio < barato) {
-            barato = item.precio;
-            masBarato = item.marca + " " + item.modelo;
-          }
-        });
-        console.log("Vehículo mas barato:", masBarato);
-      }
+const data = loadData();
 
-      function contieneY(data) {
-        data.map((item, index) => {
-          item.modelo.includes("Y") &&
-            console.log(
-              'Vehículo que contiene en el modelo la letra "Y":',
-              `${item.marca} ${item.modelo} $${item.precio}`
-            );
-        });
-      }
+function printAll(data) {
+  data.map(item => item.printData());
+}
 
-      function ordenar(data) {
+function masCaro(data) {
+  var masCaro;
+  var caro = 0;
+  data.map((item, index) => {
+    if (item.precio > caro) {
+      caro = item.precio;
+      masCaro = item.marca + " " + item.modelo;
+    }
+  });
+  console.log("Vehículo mas caro:", masCaro);
+}
 
-        const array = [];
+function masBarato(data) {
+  var masBarato;
+  var barato = 0;
+  data.map(item => {
+    if (barato == 0 || item.precio < barato) {
+      barato = item.precio;
+      masBarato = item.marca + " " + item.modelo;
+    }
+  });
+  console.log("Vehículo mas barato:", masBarato);
+}
 
-        data.map((item) => {
-          const obj = {
-            marca: item.marca,
-            modelo: item.modelo,
-            precio: item.precio
-          };
-          array.push(obj);
+function existeY(data) {
+  data.map(item => item.contieneY() );
+}
 
-          array.sort((a, b) => {
-            return b.precio - a.precio;
-          });
-        });
-        
-        const ordenado = array;
+function ordenar(data) {
+  const array = [];
+  data.map((item) => {
+    const obj = {
+      marca: item.marca,
+      modelo: item.modelo,
+      precio: item.precio
+    };
+    array.push(obj);
+    array.sort((a, b) => {return b.precio - a.precio});
+  });
+  array.map(item => console.log(item.marca + " " + item.modelo + " " + formatearPrecio(item.precio)));
+}
 
-        ordenado.map((item) => {
-          console.log(item.marca + " " + item.modelo + " " + "$" + item.precio);
-        });
-      }
+function resultadoFinal(myData) {
+  printAll(myData);
+  console.log("============================");
+  masCaro(myData);
+  masBarato(myData);
+  console.log("Vehículo que contiene en el modelo la letra 'Y':");
+  existeY(myData);
+  console.log("============================");
+  console.log("Vehiculos ordenados por precio de mayor a menor");
+  ordenar(myData);
+}
 
-      function resultadoFinal(myData) {
-        printAll(myData);
-        console.log("============================");
-        masCaro(myData);
-        masBarato(myData);
-        contieneY(myData);
-        console.log("============================");
-        console.log("Vehiculos ordenados por precio de mayor a menor");
-        ordenar(myData);
-      }
-
-      resultadoFinal(data);
-   
+resultadoFinal(data);
